981bac0d2c4926052ae5c876177692207313ab09
bart115 <120222886+bart115@users.noreply.github.com>
2024-10-29 10:19:49 +0000

    New def collect_network_metrics():

--- NMS_agent.py
+++ NMS_agent.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import psutil
 
 # Endereço do NMS_Server para enviar métricas (UDP) e alertas (TCP)
 udp_server_address = ('10.0.3.10', 1234)  # IP do servidor, porta 12345 (UDP)
@@ -27,12 +28,27 @@ def send_alert():
     finally:
         tcp_socket.close()  # Fechar a conexão TCP
 
+#Coleta as metricas do agente
+def collect_network_metrics():
+    metrics = {
+        'cpu_usage': psutil.cpu_percent(interval=1),
+        'memory_usage': psutil.virtual_memory().percent,
+        'disk_usage': psutil.disk_usage('/').percent,
+        'network_io': psutil.net_io_counters()
+    }
+    return metrics
+
 # Simulando o monitoramento de métricas
 def monitor_metrics():
-    cpu_usage = 75  # Valor de CPU inicial
+    metrics = collect_network_metrics()
+    cpu_usage = metrics['cpu_usage']  # Valor de CPU inicial
+    memory_usage = metrics['memory_usage']  # Valor de memória inicial
+    disk_usage = metrics['disk_usage']  # Valor de disco inicial
+    network_io = metrics['network_io']  # Valor de E/S de rede inicial
+
     while True:
         # Envia métricas periodicamente via UDP
-        send_metric(cpu_usage)
+        send_metric(metrics)
         
         # Verifica se o uso de CPU excede 90% e envia alerta via TCP
         if cpu_usage > 90:
@@ -43,6 +59,13 @@ def monitor_metrics():
         
         # Pausa de 5 segundos antes de enviar a próxima métrica
         time.sleep(5)
+
+# def display_metrics(metrics):
+#     print("CPU Usage: {}%".format(metrics['cpu_usage']))
+#     print("Memory Usage: {}%".format(metrics['memory_usage']))
+#     print("Disk Usage: {}%".format(metrics['disk_usage']))
+#     print("Network IO: Sent = {} bytes, Received = {} bytes".format(
+#         metrics['network_io'].bytes_sent, metrics['network_io'].bytes_recv))
 
 # Executa a monitorização das métricas
 if __name__ == "__main__":
